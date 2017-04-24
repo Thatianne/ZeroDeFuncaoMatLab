740b49a07db1a5e3722fa037328f3810092575fd
Thatianne Cristina <thatianne.cristina@hotmail.com>
2017-04-23 22:44:44 -0300

    Exibição dos valores x0 e gráfico quando não tem raiz

--- bissecao.m
+++ bissecao.m
@@ -6,8 +6,9 @@ func = '(x*x)*sin(x) + cos(x)';
 % Erro
 amplitude = 0.01;
 %quantidade máxima de operações
-qtd = 150;
-
+qtd = 20;
+%valores de x0
+xs = zeros(20, 1);
 %Recebe intervalo
 disp('Digite o valor inferior');
 a = input('');
@@ -17,7 +18,8 @@ possuiRaiz = 1;
 iteracoes = 1;
 amplitudeAtual = b -a;
 while(amplitudeAtual >= amplitude)
-    x0 = (a+b)/2;    
+    x0 = (a+b)/2;
+    xs(iteracoes) = x0;
     if(subs(func, a)*subs(func, x0) < 0)
         b = x0;
     elseif(subs(func, b)*subs(func, x0) < 0)
@@ -28,18 +30,23 @@ while(amplitudeAtual >= amplitude)
     end
     iteracoes = iteracoes + 1;
     amplitudeAtual = b - a;
+    
 end
 
 if(possuiRaiz == 1)
-    fprintf('A raiz é %d', x0)
-
-    %plotar gráfico
-    x = linspace(a-2, b+2);
-    y = ((x.*x).*sin(x)) + cos(x);
-    plot(x, y);
-    grid on;
-    xlabel('x');
-    ylabel('x^2*sin(x) + cos(x)');
+    fprintf('A raiz é %d', x0)    
+    x = linspace(a-2, b+2);    
 else
+    x = linspace(-10, 10);
     fprintf('Não possui raiz no intervalo (%d, %d)', a, b);
+end
+%plotar gráfico
+y = ((x.*x).*sin(x)) + cos(x);
+plot(x, y);
+grid on;
+xlabel('x');
+ylabel('x^2*sin(x) + cos(x)');
+
+for index = 1 : iteracoes
+    fprintf('\n%d', xs(index));
 end
